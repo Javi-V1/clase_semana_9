971582ade985392f7fd04b87af6ff151f3b4a137
joserodriguez61 <jose.rodriguez@ulatina.net>
2023-07-05 22:17:22 -0600

    Clase 8

---
+++ Hilos/Capa_Modelo/Cliente/Persona.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Capa_Modelo.Cliente
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public string Identificacion { get; set; }
+        public int Telefono { get; set; }
+        public string Tramite { get; set; }
+
+    }
+}
---
+++ Hilos/Capa_Modelo/Cola/Cola_Banco.cs
@@ -0,0 +1,17 @@
+﻿using Capa_Modelo.Nodo;
+using Capa_Modelo.Cliente;
+
+namespace Capa_Modelo.Cola
+{
+    public class Cola_Banco : Nodo<Persona, Cola_Banco>
+    {
+        public override Persona Valor { get; set; }
+        public override Cola_Banco Siguiente { get; set; }
+        
+        public Cola_Banco(Persona _valor)
+        {
+            Valor = _valor;
+            Siguiente = null;
+        }
+    }
+}
